c622228d99b7be914e948ef5e901faf1d02b8f05
Leandro Mouffron <leko.comercial@gmail.com>
2024-04-22 19:32:22 -0300

    Projeto Coder House tomando forma

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var MenuItens = document.getElementById("MenuItens");
+
+MenuItens.style.maxHeight = "0px";
+
+function menucelular(){
+    if(MenuItens.style.maxHeight == "0px"){
+        MenuItens.style.maxHeight = "200px";
+    }else{
+        MenuItens.style.maxHeight = "0px";
+    }
+}
