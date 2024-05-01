392d2bf13c07bc1cf69631b6a0683328d3198b68
Leandro Mouffron <leko.comercial@gmail.com>
2024-05-01 15:13:36 -0300

    Criação da parte de produtos de forma independente

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,22 @@ function menucelular(){
         MenuItens.style.maxHeight = "0px";
     }
 }
+
+var produtoImg = document.getElementById('produtoImg');
+var produtoMiniatura = document.getElementsByClassName("produtoMiniatura");
+
+produtoMiniatura[0].onclick = function(){
+    produtoImg.src = produtoMiniatura[0].src;
+}
+
+produtoMiniatura[1].onclick = function(){
+    produtoImg.src = produtoMiniatura[1].src;
+}
+
+produtoMiniatura[2].onclick = function(){
+    produtoImg.src = produtoMiniatura[2].src;
+}
+
+produtoMiniatura[3].onclick = function(){
+    produtoImg.src = produtoMiniatura[3].src;
+}
